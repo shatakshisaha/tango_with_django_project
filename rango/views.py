7b0d610bfef24692ca61aec073c2de467df04558
Shatakshi Saha <3070943s@student.gla.ac.uk>
2025-02-14 16:25:40 +0000

    Chapter 10 + Exercises

--- rango/views.py
+++ rango/views.py
@@ -6,23 +6,32 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
-    context_dict = {
-        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-        'categories': category_list,
-        'pages': page_list
-    }
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
-   print(request.method)
-   print(request.user)
-   return render(request, 'rango/about.html', {})
+    # Call the visitor_cookie_handler to update the session with visit count
+    visitor_cookie_handler(request)
+    
+    # Pass the visit count to the context so it can be displayed on the page
+    context_dict = {'visits': request.session['visits']}
+    
+    return render(request, 'rango/about.html', context=context_dict)
+
     
     
 def show_category(request, category_name_slug):
@@ -182,3 +191,25 @@ def user_logout(request):
     logout(request)
     # Redirect the user back to the homepage after logging out
     return redirect(reverse('rango:index'))
+ 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+    
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If more than a day has passed, increment visits
+    if (datetime.now() - last_visit_time).days > 0:
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update visits count in session
+    request.session['visits'] = visits
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,6 +18,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_AGE = 1209600
+
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
